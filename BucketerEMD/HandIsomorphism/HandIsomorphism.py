887ce1e0584c17fdde87a463c321719ebe2e36a6
chengfangjuan <xuemo89@163.com>
2019-02-18 17:15:52 +0800

    hand third

--- BucketerEMD/HandIsomorphism/HandIsomorphism.py
+++ BucketerEMD/HandIsomorphism/HandIsomorphism.py
@@ -9,10 +9,10 @@ from scipy.special import comb
 class HandIsomorphism():
 
     def __init__(self):
-        self.rank_index_map = {'A': 0, 'K': 1, 'Q': 2,
-                          'J': 3, 'T': 4, '9': 5,
-                          '8': 6, '7': 7, '6': 8,
-                          '5': 9, '4': 10, '3': 11, '2': 12}
+        self.rank_index_map = {'A': 1, 'K': 2, 'Q': 3,
+                          'J': 4, 'T': 5, '9': 6,
+                          '8': 7, '7': 8, '6': 9,
+                          '5': 10, '4': 11, '3': 12, '2': 13}
         self.suit_table = ['h', 's', 'd', 'c']
         self.suit_index_map = {'h': 0, 's': 1, 'd': 2, 'c': 3}
         self.card_to_string = CARD_TO_STRING()
@@ -46,6 +46,42 @@ class HandIsomorphism():
                     card_index = card_index + comb(M_rank[i-1], card_number-i+1)
         return card_index
 
+    #计算单个回合的牌的index，card 列表表示 如['As', 'Ah']
+    def computer_preround_card_index(self,card):
+        suit_card = dict()
+        suit_index = dict()
+        out_index = 0
+        for i in card:
+            if i[1] not in suit_card:
+                suit_card[i[1]] = []
+            suit_card[i[1]].append(i[0])
+        for id, value in suit_card.items():
+            suit_index[id] = self.computer_single_rank_card_index(value)
+
+        for id, value in suit_index.items():
+            out_index += comb(13,len(suit_card[id])) * value
+
+        return out_index
+
+    def test_pre_round_index(self):
+        all_cards = self.get_all_cards()
+        available_cards = list(combinations(all_cards,2))
+        print("available cards count:", comb(len(all_cards),2))
+        all_card_index = []
+        for card in available_cards:
+            card_ = list(card)
+            temp_index = self.computer_preround_card_index(card_)
+            print("--- card {0} --- index {1}".format(card_, temp_index))
+            if temp_index not in all_card_index:
+                all_card_index.append(temp_index)
+        print("cards index count:", len(all_card_index))
+
+
+
+
+
+
+    # 计算牌的index， card的表示是列表, [['As','Ah'],[],[],[]], 最大4个元素，表示4个回合的牌
     def computer_card_index(self, card):
         pass
 
@@ -59,3 +95,4 @@ class HandIsomorphism():
 
 if __name__ == "__main__":
     HandIsomorphism = HandIsomorphism()
+    HandIsomorphism.test_pre_round_index()
